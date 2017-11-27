9cc9b1bc28199d1e5f191f904012505e2165f31c
badboy <616860597@qq.com>
2017-11-27 10:23:10 +0800

    sev

---
+++ js/common.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+
+    // 检测到微信就提醒
+    var btn1_remind = document.getElementById('JdownApp');//下载一
+    weixinTip(btn1_remind);
+
+    
+
+});
+
+function weixinTip(ele){
+    var ua = navigator.userAgent;
+    var isWeixin = !!/MicroMessenger/i.test(ua);
+        if(isWeixin){
+            ele.onclick=function(e){
+                 e.preventDefault();
+                      e.stopPropagation();  
+                //alert(8)
+                window.event? window.event.returnValue = false : e.preventDefault();
+                document.getElementById('JweixinTip').style.display='block';
+            }
+                document.getElementById('JweixinTip').onclick=function(){
+                    this.style.display='none';
+                }   
+        }
+}
--- js/header.js
+++ js/header.js
@@ -1,45 +1,6 @@
 
 
 
-
-
-// 下载
-var u = navigator.userAgent;
-
-var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
-
-var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
-
-var a_down=document.getElementById('loadingAppGo');
-
-a_down.onclick=function(){
-
-    if(isAndroid){
-
-        this.href="http://mobileapp.jiazi-it.com/index.php?m=home&c=version&a=uploadLatestApk&aid=2";
-
-        //alert('Android')
-
-    }
-
-    else if(isiOS){
-
-        this.href="itms-apps://itunes.apple.com/app/id1182185340";
-
-        //alert('ios')
-
-    }
-
-    else{
-
-        this.href="http://mobileapp.jiazi-it.com/index.php?m=home&c=version&a=uploadLatestApk&aid=2";
-
-        //alert(89)
-
-    }
-
-};
-
 $(function () {
     $('.navShow').click(function () {
 
